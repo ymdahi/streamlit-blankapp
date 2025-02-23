3064b2394b77a7d198e54d4bf60742e290ee79dc
Yasin Dahi <ymdahi@gmail.com>
2025-02-23 01:52:33 +0000

    add streamlite_app back

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+
+
+LOGO_URL_LARGE = "media/uwlogo.png"
+st.logo(
+    LOGO_URL_LARGE,
+)
+
+
+
+st.title("aaaGenAI Lab")
+st.write(
+    "## Welcome"
+)
